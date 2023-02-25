df1824244965f86f98684bcc520b01b925e629ee
Waddah <waddah.ja@gmail.com>
2023-02-25 19:51:19 +0300

    remove commented line

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '@modules/app';
-// import { AppModule } from '../src/modules/app';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
