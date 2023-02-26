e1822b0092b0e09773d5618569531c2c77144067
Waddah <waddah.ja@gmail.com>
2023-02-26 19:45:07 +0300

    fix migration file format

--- src/database/migrations/1677424898914-CreateWordTypes.ts
+++ src/database/migrations/1677424898914-CreateWordTypes.ts
@@ -1,16 +1,21 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateWordTypes1677424898914 implements MigrationInterface {
-    name = 'CreateWordTypes1677424898914'
+  name = 'CreateWordTypes1677424898914';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "word_types" ("id" SERIAL NOT NULL, "type" character varying(100) NOT NULL, CONSTRAINT "PK_5bf5ca5cad7e26171e274ae795b" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE INDEX "IDX_de36e6f1e463de71dac56d4c4f" ON "word_types" ("type") `);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "word_types" ("id" SERIAL NOT NULL, "type" character varying(100) NOT NULL, CONSTRAINT "PK_5bf5ca5cad7e26171e274ae795b" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "IDX_de36e6f1e463de71dac56d4c4f" ON "word_types" ("type") `,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."IDX_de36e6f1e463de71dac56d4c4f"`);
-        await queryRunner.query(`DROP TABLE "word_types"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX "public"."IDX_de36e6f1e463de71dac56d4c4f"`,
+    );
+    await queryRunner.query(`DROP TABLE "word_types"`);
+  }
 }
