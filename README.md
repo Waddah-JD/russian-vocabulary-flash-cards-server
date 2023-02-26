4068ad2ee4dc0dea048d6c0e7307af4f733407fe
Waddah <waddah.ja@gmail.com>
2023-02-26 09:18:46 +0300

    create test env

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ yarn env:dev:down
 # restart develop environment
 yarn env:dev:restart
 ```
+
+### How to run tests?
+
+```bash
+# start test environment
+yarn env:test:up
+
+# run e2e tests
+yarn test:e2e
+```
--- src/modules/config/index.ts
+++ src/modules/config/index.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as BaseConfigModule } from '@nestjs/config';
 
-import { ZConfigService } from './service';
+import { ConfigService } from './service';
+
+function getEnvFileExtension(env?: string) {
+  switch (env) {
+    case 'test':
+      return 'test';
+    case 'production':
+      return 'prod';
+    case 'development':
+    default:
+      return 'dev';
+  }
+}
 
 @Module({
-  imports: [BaseConfigModule.forRoot({ envFilePath: ['.env'] })],
-  providers: [ZConfigService],
-  exports: [ZConfigService],
+  imports: [
+    BaseConfigModule.forRoot({
+      envFilePath: [`.env.${getEnvFileExtension(process.env.NODE_ENV)}`],
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
 })
 export class ConfigModule {}
