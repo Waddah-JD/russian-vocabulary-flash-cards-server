095d35cce3ac1a1f8e5ed4c95e7d00716ed06a72
Waddah <waddah.ja@gmail.com>
2023-03-05 14:40:24 +0300

    add lint-staged

--- src/database/migrations/1678001602437-AddCompoundIdConstraintToUserWords.ts
+++ src/database/migrations/1678001602437-AddCompoundIdConstraintToUserWords.ts
@@ -13,6 +13,9 @@ export class AddCompoundIdConstraintToUserWords1678001602437
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+
+
+
       `ALTER TABLE "users_words" DROP CONSTRAINT "user_id_word_id"`,
     );
   }
