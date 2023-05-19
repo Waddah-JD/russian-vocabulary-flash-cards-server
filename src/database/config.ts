27083802d0dd53fad7421861aa9334341c72f6d8
Waddah <waddah.ja@gmail.com>
2023-05-19 11:21:15 +0300

    remove logging

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,9 +3,8 @@ import * as dotenv from 'dotenv';
 import { join } from 'path';
 import { DataSource } from 'typeorm';
 
-dotenv.config({ path: `.env.${getEnvFileExtension(process.env.NODE_ENV)}` });
+dotenv.config({ path: `.env${getEnvFileExtension(process.env.NODE_ENV)}` });
 
-console.log(process.env.NODE_ENV);
 const typeOrmConnectionDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
